1dc76d44e8947f4d85f941ad9b181dc70050d2e8
renasboy <renasboy@gmail.com>
2017-08-02 21:13:02 +0200

    Raised the gas on deploy

--- bin/deploy.tpl.js
+++ bin/deploy.tpl.js
@@ -5,7 +5,7 @@ var owner_fee = OWNER_FEE;
 var abi = ABI;
 var code = "0xCODE";
 
-web3.eth.contract(abi).new(fee, jackpot, owner_fee, { from: owner, data: code, gas: 1000000 });
+web3.eth.contract(abi).new(fee, jackpot, owner_fee, { from: owner, data: code, gas: 3000000 });
 
 miner.start(1);
 admin.sleepBlocks(1);
